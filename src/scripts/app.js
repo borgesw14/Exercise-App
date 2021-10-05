76fcbc5a0e308ba576d1f17a7488d78cd4487d65
borgesw14 <borgesw14@gmail.com>
2021-10-04 23:07:46 -0400

    updated app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,9 +6,6 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello, World!')
 })
-.get('/newpaltz', (req, res) => {
-    res.send('Hello, New Paltz')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
