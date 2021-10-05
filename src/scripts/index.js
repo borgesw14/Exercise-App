e2418305e49439011769c978812f00f3fc0ddd31
borgesw14 <borgesw14@gmail.com>
2021-10-04 22:50:55 -0400

    express server

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,18 @@
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+.get('/newpaltz', (req, res) => {
+    res.send('Hello, New Paltz')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 //tabs
 const tabs = document.querySelectorAll('.tabs li');
