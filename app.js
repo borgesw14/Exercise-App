38a6d9ee7bde371a5e37e4f1ec1b5b8ca26ab459
William Borges <borgesw14@gmail.com>
2021-10-06 02:24:46 -0400

    fixed port

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'docs')));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'scripts')));
 app.use(express.static(path.join(__dirname, 'styles')));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
