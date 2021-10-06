f0c2a85c1144bdcbcdfbad5050e32353624cc9ed
William Borges <borgesw14@gmail.com>
2021-10-06 02:21:17 -0400

    fixed website not loading from node app

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
-
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'docs')));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'scripts')));
+app.use(express.static(path.join(__dirname, 'styles')));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
