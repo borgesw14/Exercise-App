f50f71b97e423a08258bc6d5f80cef8caee8a509
William Borges <borgesw14@gmail.com>
2021-10-07 21:56:28 -0400

    landing page with two components

---
+++ client/src/components/Landing-Hero.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <section id="header-hero" class="hero  is-halfheight is-light ">
+            <div class="hero-body">
+                <div class="container has-text-left ">
+    
+                    <p class="title has-text-white is-size-1">
+                        Your Life
+                    </p>
+                    <p class="subtitle has-text-white is-size-3">
+                        but Fit
+                    </p>
+                    <button class="button is-dark is-rounded">
+                        Sign Up
+                    </button>
+                    <button class="button is-white is-rounded is-outlined">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    header-hero {
+        background: '@/assets/equipment.jpeg';
+    }
+</style>
+
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <landing-hero/>
+    <div class="section">
+      <testimonials/>
+    </div>
+    
   </div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import LandingHero from '../components/Landing-Hero.vue';
+import Testimonials from '../components/Testimonials.vue';
 export default {
+  
   name: 'Home',
   components: {
-    HelloWorld
+    LandingHero,
+    Testimonials
   }
 }
 </script>
