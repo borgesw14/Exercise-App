d920da5bcb1ca59a74853dc875779da3b2153eb4
William Borges <borgesw14@gmail.com>
2021-10-14 09:58:05 -0400

    heroku push

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express()
 const PORT = process.env.PORT || 8080;
 
-app.use(serveStatic(path.join(__dirname, '../docs')));
+app.use('/', serveStatic(path.join(__dirname, '../docs')));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
