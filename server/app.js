0f8bb336d850e6ad7593e85b1bcbb2d68afa6531
William Borges <borgesw14@gmail.com>
2021-11-15 22:22:45 -0500

    connected to mongo database amd api routing

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 
 const express = require('express');
 const serveStatic = require('serve-static');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
+
+const {PORT, mongoUri} = require('./config');
+const postsRoutes = require('./routes/api/posts')
+
 const app = express()
-const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
 
 app.use(serveStatic(path.join(__dirname, '../docs')));
+app.use('/api/posts', postsRoutes)
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB database Connnected...')).catch((err) => console.log(err))
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
